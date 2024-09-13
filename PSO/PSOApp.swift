7dfee348d8edebc1d5dbc5d75d5163d48cf21aa7
carlneto <carlneto@icloud.com>
2024-09-13 11:53:46 +0100

    Initial Commit

---
+++ PSO/PSOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSOApp.swift
+//  PSO
+//
+//  Created by Carlos Neto on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PSOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
